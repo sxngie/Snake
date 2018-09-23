d20057e36062d19ac934fccbb794389c0dd7430b
adel <adel@192.168.2.14>
2018-09-22 20:41:52 -0700

    Se modifico el color del background, linea y grid size

--- src/Display/DisplayScreen.java
+++ src/Display/DisplayScreen.java
@@ -17,7 +17,8 @@ public class DisplayScreen {
     private String title;
     private int width, height;
 
-    public DisplayScreen(String title, int width, int height){
+    
+	public DisplayScreen(String title, int width, int height){
         this.title = title;
         this.width = width;
         this.height = height;
@@ -25,16 +26,15 @@ public class DisplayScreen {
 
 
         createDisplay();
-    }
-
-    private void createDisplay(){
+	}
+	private void createDisplay(){
         frame = new JFrame(title);
         frame.setSize(width, height);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
-        frame.setBackground(Color.black);
+        frame.setBackground(Color.blue);
 
         try {
             frame.setIconImage(ImageIO.read(new File("res/Sheets/icon.png")));
@@ -47,7 +47,7 @@ public class DisplayScreen {
         canvas.setMaximumSize(new Dimension(width, height));
         canvas.setMinimumSize(new Dimension(width, height));
         canvas.setFocusable(false);
-        canvas.setBackground(Color.black);
+        canvas.setBackground(Color.magenta);
 
         frame.add(canvas);
         frame.pack();
